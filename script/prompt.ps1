802e76323d568ba2f444bab1a82fc15ae967a3f4
Prabhat Agarwal <prabhat.agarwal@siemens.com>
2021-10-13 17:02:04 +0530

    added color scheme and helm symbol in prompt

--- script/prompt.ps1
+++ script/prompt.ps1
@@ -7,8 +7,9 @@ function kubeOff {
 function kubeOn(){
     function global:Prompt {
         $ESC = [char]27
+        $symbol = [char]0x2388
         $context = $(kubectl config current-context)
         $namespace = $(kubectl config view --minify --output 'jsonpath={..namespace}')
-        "$ESC[38;2;0;255;128mctx:$context|$ESC[38;2;0;204;204mns:$namespace>"
+        "$symbol|$ESC[38;2;0;255;128mctx:$context$ESC[0m|$ESC[38;2;0;204;204mns:$namespace$ESC[0m> "
     }
 }
